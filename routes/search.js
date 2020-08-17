1458fd71b21e3cd378c03470fd53753feac0850b
Odie Edo-Osagie <O.Edo-Osagie@uea.ac.uk>
2020-08-17 13:29:25 +0100

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -20,80 +20,75 @@ router.get('/', function(req, res) {
 
 	        $ = cheerio.load(body);
 
-	        $('#mangaresults .mangaresultitem .mangaresultinner').each(function(result) {
-				var resultName = null;
-				var resultUrl = null;
-				var resultFullUrl = null;
-				var thumb = null;
-				var chapters = null;
-				var type = null;
-				var genre = null;
+					$('.d54').each(function(i){
+						rows = $(this).find('tr').children();
+						rows.each((i, tr) => {
+		          const children = $(tr).children();
+		          // console.log(children.find('.d57 a').text());
+							// console.log(children.find('.d57 a').attr('href'));
+							var resultName = null;
+							var resultUrl = null;
+							var resultFullUrl = null;
+							var thumb = null;
+							var chapters = null;
+							var type = null;
+							var genre = null;
 
-				$(this).find('a').each(function() {
-					resultName = $(this).text();
-					resultUrl = $(this).attr('href');
-					resultFullUrl = rootUrl + resultUrl;
-    			});
 
-				$(this).find('.imgsearchresults').each(function() {
-					thumb = $(this).css('background-image');
-					thumb = thumb.replace('url(\'','').replace('\')','');
-    			});
+							children.each(function(i){
+								if($(this).attr("class") == "d57"){
+									resultName = $(this).find('a').text();
+									resultUrl = $(this).find('a').attr('href');
+									resultFullUrl = rootUrl + resultUrl;
+								}
 
-    			$(this).find('.chapter_count').each(function() {
-    				chapters = $(this).text();
-    			});
+								if($(this).attr("class") == "d58"){
+									chapters = $(this).text();
+								}
 
-    			$(this).find('.manga_type').each(function() {
-    				type = $(this).text();
-    			});
+								if($(this).attr("class") == "d59"){
+									type = $(this).text();
+								}
 
-    			$(this).find('.manga_genre').each(function() {
-    				genre = $(this).text();
-    			});
+								if($(this).attr("class") == "d60"){
+									genre = $(this).text();
+								}
 
-				var result = {
-                    "resultName": resultName,
-                    "resultUrl": resultUrl,
-                    "resultFullUrl" : resultFullUrl,
-                    "resultSearchThumbImageUrl" : thumb,
-										"resultThumbImageUrl" : thumb.replace('r0.', 'l0.'),
-                    "resultChapters" : chapters,
-                    "resultType" : type,
-                    "resultGenre" : genre
-                };
+								if($(this).attr("data-src")){
+									thumb = "https:" + $(this).attr("data-src");
+								}
+							});
 
-                results.push(result);
-	        });
+							if(resultName){
+								var result = {
+														"resultName": resultName,
+														"resultUrl": resultUrl,
+														"resultFullUrl" : resultFullUrl,
+														"resultSearchThumbImageUrl" : thumb,
+														"resultThumbImageUrl" : thumb,
+														"resultChapters" : chapters,
+														"resultType" : type,
+														"resultGenre" : genre
+												};
 
-			var pages = 0;
+												results.push(result);
+							}
 
-	        $('#sp').each(function(result) {
-				$(this).find('a').each(function() {
-					var pageUrl = $(this).attr('href');
+		        });
+					});
 
-					if (pageUrl) {
-						console.log(pageUrl);
-					}
-					// console.log($(this).text());
-					pages = pageUrl;
-				});
-			});
-
-	        if (results.length < itemsPerPage) {
-	        	pages = (results.length == 0 ? 0 : 1);
-	        } else {
-	        	pages = (pages / itemsPerPage) + 1;
-	        }
-
-	        var searchResults = {
+					var searchResults = {
 	        	"searchTerm" : searchTerm,
 	        	"resultCount" : results.length,
-	        	"resultPageCount" : pages,
+	        	"resultPageCount" : 1,
 	        	"results": results
 	        };
 
 	        res.send(JSON.stringify(searchResults));
+
+
+
+
 	    });
 	} else {
 		res.send('no searchTerm');
